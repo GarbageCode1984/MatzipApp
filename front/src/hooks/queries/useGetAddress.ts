083cc5c87dbda27480ed494f6bd1ba3a1093cc5c
GarbageCode1984 <rkqlwl1984@gmail.com>
2024-11-28 23:10:23 +0900

    useGetAddress 주소 안 찍히는 문제 수정

--- front/src/hooks/queries/useGetAddress.ts
+++ front/src/hooks/queries/useGetAddress.ts
@@ -8,7 +8,7 @@ function useGetAddress(location: LatLng) {
     const [result, setResult] = useState('');
 
     useEffect(() => {
-        async () => {
+        (async () => {
             try {
                 const {data} = await axios.get(
                     `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&result_type=street_address|route|political&key=AIzaSyABRIr2lJ4vwBIuFSqrKITzO21JMSc2xoA&language=ko`
@@ -16,14 +16,11 @@ function useGetAddress(location: LatLng) {
                 const address = data.results.length
                     ? data.results[0].formatted_address
                     : `${latitude.toFixed(4)}, ${longitude.toFixed(4)}`;
-
-                console.log('address', address);
                 setResult(address);
             } catch (error) {
-                console.log('error', error);
                 setResult(errorMessages.CANNOT_GET_ADDRESS);
             }
-        };
+        })();
     }, [latitude, longitude]);
 
     return result;
