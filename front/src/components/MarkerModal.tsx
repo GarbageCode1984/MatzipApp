91ff147f60cac013569e7917904f8f3ca588b84e
GarbageCode1984 <rkqlwl1984@gmail.com>
2024-12-27 01:18:48 +0900

    markermodal

--- front/src/components/MarkerModal.tsx
+++ front/src/components/MarkerModal.tsx
@@ -1,7 +1,8 @@
 import {colors} from '@/constants';
 import useGetPost from '@/hooks/queries/useGetPost';
 import React from 'react';
-import {Modal, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {Image, Modal, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import CustomMarker from './CustomMarker';
 
 interface MarkerModalProps {
     markerId: number | null;
@@ -16,7 +17,24 @@ function MarkerModal({markerId, isVisible, hide}: MarkerModalProps) {
         <Modal visible={isVisible} transparent={true} animationType="slide">
             <SafeAreaView style={styles.optionBackground} onTouchEnd={hide}>
                 <View style={styles.cardContainer}>
-                    <Text>{post?.title}</Text>
+                    <View style={styles.cardInner}>
+                        <View style={styles.cardAlign}>
+                            {post?.images.length > 0 && (
+                                <View style={styles.imageContainer}>
+                                    <Image
+                                        style={styles.image}
+                                        source={{uri: post.images[0]?.uri}}
+                                        resizeMode="cover"
+                                    />
+                                </View>
+                            )}
+                            {post?.images.length === 0 && (
+                                <View style={[styles.imageContainer, styles.emptyImageContainer]}>
+                                    <CustomMarker color={post.color} score={post.score} />
+                                </View>
+                            )}
+                        </View>
+                    </View>
                 </View>
             </SafeAreaView>
         </Modal>
@@ -30,6 +48,43 @@ const styles = StyleSheet.create({
     },
     cardContainer: {
         backgroundColor: colors.WHITE,
+        margin: 10,
+        borderRadius: 20,
+        shadowColor: colors.BLACK,
+        shadowOffset: {width: 3, height: 3},
+        shadowOpacity: 0.2,
+        elevation: 1,
+        borderColor: colors.GRAY_500,
+        borderWidth: 1.5,
+    },
+    cardInner: {
+        padding: 20,
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    cardAlign: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    imageContainer: {
+        width: 70,
+        height: 70,
+        borderRadius: 35,
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        borderRadius: 35,
+    },
+    emptyImageContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: colors.GRAY_200,
+        borderRadius: 35,
+        borderWidth: 1,
     },
 });
 
